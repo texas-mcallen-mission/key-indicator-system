01818b765170066483d3a257d061f8a4ed17cc5e
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-01-19 11:26:35 -0600

    Moved driveHandler configurations to head.js, and removed unncessesary code from driveHandler

--- head.js
+++ head.js
@@ -4,9 +4,6 @@
 */
 
 
-// THIS IS A TEST OF THE CONTINUOUS BROADCAST SYSTEM. BEEEEEEEP
-// easily identifiable line for debugging purposes
-
 
 //Static vars
   //let MASTER_SHEET_ID = "1P9tnkUKI2GRf--7O1rwYDF1oV8dgz2F3fa8qQGd3Jwk";
@@ -52,6 +49,22 @@
 
 //End
 
+
+// DRIVEHANDLER CONFIGS:
+/* this changes the name of the report folder.  Because this system uses 
+folder ID's, if you want to change the name of the folder and have the 
+changes spread across everything that already exists, you'll either have 
+to change the name of the folder itself *OR* just delete everything and 
+refresh it
+*/
+const REPORT_FOLDER_NAME = "Reports"
+/*
+// this flag lets you include a string at the end of the name of the
+report to make it easier to know where you are this is less important
+if you don't have a zone named the same thing as a district as an area,
+which is just lame anywho
+*/
+const INCLUDE_SCOPE_IN_FOLDER_NAME = false
 
 
 
