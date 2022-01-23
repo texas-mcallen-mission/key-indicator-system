55583d34d5cf7f7148b69ddb768efed0fcaa537f
than2000 <nathaniel.gerlek@gmail.com>
2022-01-22 17:32:41 -0800

    Created aa-shortcuts.js

---
+++ aa-shortcuts.js
@@ -0,0 +1,19 @@
+// Shortcut functions just to make full-system testing from GAS easier
+
+function run_updateDataSheet() {
+    updateDataSheet();
+}
+
+function run_importContacts() {
+    importContacts();
+}
+
+function run_shareFileSys() {
+    shareFileSys();
+}
+
+function run_updateReports() {
+    updateAreaReports();
+    updateDistrictReports();
+    updateZoneReports();
+}
