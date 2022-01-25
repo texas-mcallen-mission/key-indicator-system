c7e663169d24ffb33c00036b5340f97bf1ce2d50
Than2000 <nathaniel.gerlek@gmail.com>
2022-01-25 11:14:06 -0800

    Delete tsignore-appsscript-classes.js
    Deleted and added to .gitignore

--- tsignore-appsscript-classes.js
+++
@@ -1,8 +0,0 @@
-//@ts-nocheck
-//Dummy definitions for GAS classes so typescript doesn't get upset about them.
-let Logger;
-let SpreadsheetApp;
-let CacheService;
-let DriveApp;
-let Drive;
-let ContactsApp;
