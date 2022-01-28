f969b5d81617f9f3a8ba9c44ed0b907da3fde855
than2000 <nathaniel.gerlek@gmail.com>
2022-01-27 21:52:33 -0800

    outlined finding area names item

--- updateForm.js
+++ updateForm.js
@@ -18,6 +18,16 @@ function updateForm() {
     let form = FormApp.openById(formId);
 
     // find area names element
+    let items = form.getItems(itemType);
+    let areaNamesItem;
+    for (let item of items) {
+        if (/*is the right item*/) {
+            areaNamesItem = item;
+            break;
+        }
+    }
+    if (typeof areaNamesItem == 'undefined')
+        throw `Couldn't find an Area Names question in the Google Form!`
 
     // update area names
 
