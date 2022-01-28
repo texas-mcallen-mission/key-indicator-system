0024f52ef1a94f0b91a98a9e68027f98d6572ea8
than2000 <nathaniel.gerlek@gmail.com>
2022-01-27 22:07:14 -0800

    Limited checker to pull list items

--- updateForm.js
+++ updateForm.js
@@ -18,7 +18,7 @@ function updateForm() {
     let form = FormApp.openById(formId);
 
     // find area names element
-    let items = form.getItems(itemType);
+    let items = form.getItems(FormApp.ItemType.LIST);
     let areaNamesItem;
     for (let i = 0; i < items.length && typeof areaNamesItem == 'undefined'; i++) {
         if (items[i].asListItem().getTitle() == CONFIG.AREA_NAME_QUESTION_TITLE) {
