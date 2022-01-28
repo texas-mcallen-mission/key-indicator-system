5e8cab2a9ef1444e515814566673ca783699b52f
than2000 <nathaniel.gerlek@gmail.com>
2022-01-27 19:32:34 -0800

    Added .tsignore stub and added it to .gitignore

---
+++ tsignore-appsscript-classes.js
@@ -0,0 +1,9 @@
+//@ts-nocheck
+//Dummy definitions for GAS classes so typescript doesn't get upset about them.
+let Logger;
+let SpreadsheetApp;
+let CacheService;
+let DriveApp;
+let Drive;
+let ContactsApp;
+//cosa
