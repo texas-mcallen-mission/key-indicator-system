74327b2b6f016a8dc43e786587870c26426c395d
Than2000 <nathaniel.gerlek@gmail.com>
2022-02-01 07:54:21 -0800

    Delete tsignore-appsscript-classes.js

--- tsignore-appsscript-classes.js
+++
@@ -1,9 +0,0 @@
-//@ts-nocheck
-//Dummy definitions (stubs) for GAS classes so typescript doesn't get upset about them.
-let Logger;
-let SpreadsheetApp;
-let CacheService;
-let DriveApp;
-let Drive;
-let ContactsApp;
-//cosa
