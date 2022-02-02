24528e1575f846e6c12f2ad22dd7c39144a0e129
than2000 <nathaniel.gerlek@gmail.com>
2022-02-01 21:09:51 -0800

    Add setting of headers when adding cols

--- df-sheetData.js
+++ df-sheetData.js
@@ -271,6 +271,8 @@ class RawSheetData {
         if (this.hasKey(key))
             throw `Potential data collision! Tried to add key '${key}' to index ${index} in sheet ${this.getTabName()}, but that key already exists at index ${this.getIndex(key)}`;
 
+        this.getSheet().getRange(this.getHeaderRow(), index).setValue(header);
+
         this.keyToIndex[key] = index;
         // @ts-ignore
         this.indexToKey[index] = key;
