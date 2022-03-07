c038a50c8313b0352c30cae0ef66b65101af3197
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-03-06 22:24:28 -0600

    prepping for scheduler push

---
+++ dataFlow/updateForm.js
@@ -0,0 +1,47 @@
+/*
+        updateForm.js
+        Updating the Google Form automatically - area names, etc.
+*/
+
+/**
+ * Updates the Google Form for the Key Indicators for Conversion Report, such as updating the list of area names. Also deletes old form responses from the Google Form (not from the Sheet) if the config is set.
+ */
+function updateForm() {
+
+    let allSheetData = constructSheetData();
+    let cSheetData = allSheetData.contact;
+    let areaNames = cSheetData.getAllOfKey('areaName');
+    areaNames.unshift("I can't find the right area name!");
+
+    let form = FormApp.openById(CONFIG.docIds_kicFormId);
+
+    //Find Area Names question
+    let items = form.getItems(FormApp.ItemType.LIST);
+
+    //For each item, or until the right item is found
+    for (let i = 0; i < items.length && typeof areaNamesItem == 'undefined'; i++) {
+        if (items[i].asListItem().getTitle() == CONFIG.general_areaNameQuestionTitle) {
+            var areaNamesItem = items[i];
+        }
+    }
+
+    if (typeof areaNamesItem == 'undefined')
+        throw "Couldn't find an Area Names question in the Google Form!";
+
+    areaNamesItem.asListItem().setChoiceValues(areaNames);
+
+
+
+    //Delete old form responses
+    if (CONFIG.general_deleteOldResponsesAgeLimit > 0) {
+        for (let formResponse of form.getResponses()) {
+            let tstamp = formResponse.getTimestamp();
+            if (tstamp == null) continue; //Skip responses that haven't been submitted yet
+
+            let ageInDays = Math.floor(tstamp.getTime() / (1000 * 60 * 60 * 24));
+            if (ageInDays > CONFIG.general_deleteOldResponsesAgeLimit) {
+                form.deleteResponse(formResponse.getId());
+            }
+        }
+    }
+}
