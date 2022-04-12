dbe0b15de62a5fe6a23f9e771b4c48cea6e47c4a
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-04-11 20:39:54 -0500

    Added calculation for sheet id's - largely untested, who knows if it will work or not.

--- reports/sharding-tests.ts
+++ reports/sharding-tests.ts
@@ -1,5 +1,15 @@
 // Goal:  Figure out how to split the reportCreator report generators into multiple functions so that I can make the effective update time much smaller.
 
+// interface fsEntry {
+//     folderName: string,
+//     parentFolder: string,
+//     folderId: string,
+//     sheetID1: string,
+//     sheetID2: string,
+//     areaID: string,
+//     folderBaseName: string,
+//     seedId: string,
+// }
 
 function updateShards() {
     let NUMBER_OF_SHARDS = 4
@@ -10,9 +20,9 @@ function updateShards() {
         // creates a object to += counts on, so that values already assigned a particular shard won't be as likely to get shifted to a different shard.
         
         let shardCounter: {} = {
-            "0": 0,
+            "1":0
         }
-        for (let i = 0; i < NUMBER_OF_SHARDS; i++){
+        for (let i = 1; i < NUMBER_OF_SHARDS; i++){
             shardCounter[i.toString()] = 0
         }
         /* loop 1:
@@ -22,7 +32,8 @@ function updateShards() {
             NUMBER_OF_SHARDS shall be 1-indexed- anything 
         */
         for (let entry in filesystems[fs].sheetData) {
-            console.log(entry.folderBaseName, entry.seedId, typeOf entry.seedId)
+            // let Testentry: fsEntry = entry
+            console.log(entry.folderBaseName, entry.seedId, typeof entry.seedId)
             if (entry.seedId <= 0 || entry.seedId == "" || entry.seedId == undefined) {
                 entry.seedId = 0
 
@@ -31,8 +42,35 @@ function updateShards() {
             }
         }
         // this should work, I think.
-        console.log(shardCounter)
+        
+        
+        /* Loop 2:
+            1. Assigns shard numbers to things that don't have valid shard id's
+                Attempts to do so evenly by assigning to the shard with the lowest count on it
+        */
+        
+        for (let entry in filesystems[fs].sheetData) {
+            if (entry.seedId <= 0 || entry.seedId > NUMBER_OF_SHARDS) {
+                let shardString = getKeyWithSmallestValue(shardCounter) 
+                shardCounter[shardString] += 1
+                entry.seedId = shardString
+            }
+        }
+        // send data to display:
+        //filesystems[filesystem].fsData.setData(filesystems[filesystem].sheetData);
+        filesystems[fs].fsData.setData(filesystems[fs].sheetData)
     }
 
 
+}
+
+function getKeyWithSmallestValue(shardCounter) {
+    let returnKey = "1"
+
+    for (let key of shardCounter) {
+        if (shardCounter[key] < shardCounter[returnKey]) {
+            returnKey = key
+        }
+    }
+    return returnKey
 }
