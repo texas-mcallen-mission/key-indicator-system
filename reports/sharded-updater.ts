30737b2e753828edd7a2a4a008994b4d588fb7c3
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-04-23 19:39:42 -0500

    changed the way that we run shard updates

--- reports/sharded-updater.ts
+++ reports/sharded-updater.ts
@@ -5,23 +5,75 @@ function testShardUpdater1() {
 }
 
 function testShardUpdater2() {
-    updateShard("Area");
+    updateShard("District");
 }
 
 function testShardUpdater3() {
     updateShard("Zone")
 }
 
-function testShardUpdater4() {
-    updateShard("Zone");
+function updateShard1_Area() {
+    let scope = "Area";
+    let targetShard = "1";
+    meta_runner(scope, triggerTypes.timeBased, targetShard, true);
+}
+function updateShard2_Area() {
+    let scope = "Area";
+    let targetShard = "2";
+    meta_runner(scope, triggerTypes.timeBased, targetShard, true);
+}
+function updateShard3_Area() {
+    let scope = "Area";
+    let targetShard = "3";
+    meta_runner(scope, triggerTypes.timeBased, targetShard, true);
+}
+function updateShard4_Area() {
+    let scope = "Area";
+    let targetShard = "4";
+    meta_runner(scope, triggerTypes.timeBased, targetShard, true);
+}
+function updateShard1_District() {
+    let scope = "District";
+    let targetShard = "1";
+    meta_runner(scope, triggerTypes.timeBased, targetShard, true);
+}
+function updateShard2_District() {
+    let scope = "District";
+    let targetShard = "2";
+    meta_runner(scope, triggerTypes.timeBased, targetShard, true);
+}
+function updateShard3_District() {
+    let scope = "District";
+    let targetShard = "3";
+    meta_runner(scope, triggerTypes.timeBased, targetShard, true);
+}
+function updateShard4_District() {
+    let scope = "District";
+    let targetShard = "4";
+    meta_runner(scope, triggerTypes.timeBased, targetShard, true);
+}
+function updateShard1_Zone() {
+    let scope = "Zone";
+    let targetShard = "1";
+    meta_runner(scope, triggerTypes.timeBased, targetShard, true);
+}
+function updateShard2_Zone() {
+    let scope = "Zone";
+    let targetShard = "2";
+    meta_runner(scope, triggerTypes.timeBased, targetShard, true);
+}
+function updateShard3_Zone() {
+    let scope = "Zone";
+    let targetShard = "3";
+    meta_runner(scope, triggerTypes.timeBased, targetShard, true);
+}
+function updateShard4_Zone() {
+    let scope = "Zone";
+    let targetShard = "4";
+    meta_runner(scope, triggerTypes.timeBased, targetShard, true);
 }
 
-function testShardUpdater5() {
-    updateShard("District");
-}
-function testShardUpdater6() {
-    updateShard("District");
-}
+
 
 function updateShard(scope: filesystemEntry["fsScope"]) {
     // this implementation is somewhat dumb and essentially requires things to take more than a minute to update to hit shards further down the line.
@@ -35,11 +87,11 @@ function updateShard(scope: filesystemEntry["fsScope"]) {
     let worked = false
     let availableShards = []
     // TODO: make this a little smarter so that the first group of seeds isn't the only one getting updated in weird unloaded edge cases
-    for (let i = 1; i <= CONFIG.fileSystem.shardManager.number_of_shards; i++){
+    for (let i = 1; i <= INTERNAL_CONFIG.fileSystem.shardManager.number_of_shards; i++){
         if (currentState[scope][i.toString()] == false) {
             availableShards.push(i.toString())
         } else {
-            if (i == CONFIG.fileSystem.shardManager.number_of_shards) {
+            if (i == INTERNAL_CONFIG.fileSystem.shardManager.number_of_shards) {
                 console.log("Nothing available to update on scope" + scope)
                 return // breaks function, so we don't run anything else
             }
@@ -71,7 +123,7 @@ interface shardValueSet {
 
 
 function createShardValues():manyShardValues {
-    let maxShards = CONFIG.fileSystem.shardManager.number_of_shards
+    let maxShards = INTERNAL_CONFIG.fileSystem.shardManager.number_of_shards
     let output: manyShardValues = {}
     for (let scope of ["Zone", "District", "Area"]) {
         output[scope] = {}
@@ -85,7 +137,7 @@ function createShardValues():manyShardValues {
 
 function loadCacheValues() {
     let cache = CacheService.getScriptCache()
-    let cacheValues = cache.get(CONFIG.fileSystem.shardManager.shard_cache_base_key)
+    let cacheValues = cache.get(INTERNAL_CONFIG.fileSystem.shardManager.shard_cache_base_key)
     let cacheOutput: manyShardValues = {}
     if (cacheValues == null || cacheValues == "" || typeof cacheValues == undefined) {
         cacheOutput = createShardValues()
@@ -112,11 +164,11 @@ function testShardCache() {
 
 function clearShardCache() {
     let cache = CacheService.getScriptCache();
-    cache.remove(CONFIG.fileSystem.shardManager.shard_cache_base_key);
+    cache.remove(INTERNAL_CONFIG.fileSystem.shardManager.shard_cache_base_key);
 }
 
 function setCacheValues(shardCacheObject: manyShardValues) {
     let cacheValue = JSON.stringify(shardCacheObject)
     let cache = CacheService.getScriptCache()
-    cache.put(CONFIG.fileSystem.shardManager.shard_cache_base_key, cacheValue,
+    cache.put(INTERNAL_CONFIG.fileSystem.shardManager.shard_cache_base_key, cacheValue,
 }
