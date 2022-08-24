1ce7520bdb053eb41bfe623655f929ef22db3ace
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-08-23 21:39:25 -0500

    Added config options to new secret, and removed headerTest

--- KIC-config.ts
+++ KIC-config.ts
@@ -504,90 +504,90 @@ function getSheetDataConfig(): { local: manySheetDataEntries } {
 
                 },
             },
-            headerTest: {
-                tabName: "headerTest",
-                headerRow: 1,
-                includeSoftcodedColumns: true,
-                sheetId: CONFIG.dataFlow.sheetTargets.headerTest,
-                initialColumnOrder: {
-                    areaName: 0,
-                    log: 1,
-                    areaEmail: 2,
-                    isDuplicate: 3,
-                    formTimestamp: 4, //form data
-                    areaID: 5,
-                    kiDate: 6, //form data
+            // headerTest: { // Removed: I don't need this, and it makes it make a new tab that I can't delete.  Was also only for debugging anywho...
+            //     tabName: "headerTest",
+            //     headerRow: 1,
+            //     includeSoftcodedColumns: true,
+            //     sheetId: CONFIG.dataFlow.sheetTargets.headerTest,
+            //     initialColumnOrder: {
+            //         areaName: 0,
+            //         log: 1,
+            //         areaEmail: 2,
+            //         isDuplicate: 3,
+            //         formTimestamp: 4, //form data
+            //         areaID: 5,
+            //         kiDate: 6, //form data
 
-                    np: 7, //form data
-                    sa: 8, //form data
-                    bd: 9, //form data
-                    bc: 10, //form data
-                    rca: 11, //form data
-                    rc: 12, //form data
-                    serviceHrs: 14, //form data
+            //         np: 7, //form data
+            //         sa: 8, //form data
+            //         bd: 9, //form data
+            //         bc: 10, //form data
+            //         rca: 11, //form data
+            //         rc: 12, //form data
+            //         serviceHrs: 14, //form data
 
-                    name1: 15,
-                    position1: 16,
-                    isTrainer1: 17,
-                    name2: 18,
-                    position2: 19,
-                    isTrainer2: 20,
-                    name3: 21,
-                    position3: 22,
-                    isTrainer3: 23, // hello, update!
+            //         name1: 15,
+            //         position1: 16,
+            //         isTrainer1: 17,
+            //         name2: 18,
+            //         position2: 19,
+            //         isTrainer2: 20,
+            //         name3: 21,
+            //         position3: 22,
+            //         isTrainer3: 23, // hello, update!
 
-                    cki: 13, //form data
-                    // super confused
-                    districtLeader: 24,
-                    zoneLeader1: 25,
-                    zoneLeader2: 26,
-                    zoneLeader3: 27,
-                    stl1: 28,
-                    stl2: 29,
-                    stl3: 30,
-                    stlt1: 31,
-                    stlt2: 32,
-                    stlt3: 33,
-                    assistant1: 34,
-                    assistant2: 35,
-                    assistant3: 36,
+            //         cki: 13, //form data
+            //         // super confused
+            //         districtLeader: 24,
+            //         zoneLeader1: 25,
+            //         zoneLeader2: 26,
+            //         zoneLeader3: 27,
+            //         stl1: 28,
+            //         stl2: 29,
+            //         stl3: 30,
+            //         stlt1: 31,
+            //         stlt2: 32,
+            //         stlt3: 33,
+            //         assistant1: 34,
+            //         assistant2: 35,
+            //         assistant3: 36,
 
-                    district: 37,
-                    zone: 38,
-                    unitString: 39,
-                    hasMultipleUnits: 40,
-                    languageString: 41,
-                    isSeniorCouple: 42,
-                    isSisterArea: 43,
-                    hasVehicle: 44,
-                    vehicleMiles: 45,
-                    vinLast8: 46,
-                    aptAddress: 47,
+            //         district: 37,
+            //         zone: 38,
+            //         unitString: 39,
+            //         hasMultipleUnits: 40,
+            //         languageString: 41,
+            //         isSeniorCouple: 42,
+            //         isSisterArea: 43,
+            //         hasVehicle: 44,
+            //         vehicleMiles: 45,
+            //         vinLast8: 46,
+            //         aptAddress: 47,
 
-                    "bap-self-ref": 48,
-                    "bap-street": 49,
-                    "bap-ward-activity-or-event": 50,
-                    "bap-ref-recent-convert": 51,
-                    "bap-ref-part-member": 52,
-                    "bap-ref-other-member": 53,
-                    "bap-ref-teaching-pool": 54,
-                    "bap-ref-other-non-member": 55,
-                    "bap-fb-mission": 56,
-                    "bap-fb-personal": 57,
-                    "bap-family-history": 58,
-                    "bap-taught-prev": 59,
-                    "fb-role": 60,
-                    "fb-ref-ysa": 61,
-                    "fb-ref-asl": 62,
-                    "fb-ref-service": 63,
-                    "fb-ref-laredo-spa": 64,
-                    "fb-ref-laredo-eng": 65,
-                    "fb-ref-rgv-spa": 66,
-                    "fb-ref-rgv-eng": 67,
-                    "fb-ref-corpus": 68,
-                    "fb-ref-personal": 69,
-                },
-            },
+            //         "bap-self-ref": 48,
+            //         "bap-street": 49,
+            //         "bap-ward-activity-or-event": 50,
+            //         "bap-ref-recent-convert": 51,
+            //         "bap-ref-part-member": 52,
+            //         "bap-ref-other-member": 53,
+            //         "bap-ref-teaching-pool": 54,
+            //         "bap-ref-other-non-member": 55,
+            //         "bap-fb-mission": 56,
+            //         "bap-fb-personal": 57,
+            //         "bap-family-history": 58,
+            //         "bap-taught-prev": 59,
+            //         "fb-role": 60,
+            //         "fb-ref-ysa": 61,
+            //         "fb-ref-asl": 62,
+            //         "fb-ref-service": 63,
+            //         "fb-ref-laredo-spa": 64,
+            //         "fb-ref-laredo-eng": 65,
+            //         "fb-ref-rgv-spa": 66,
+            //         "fb-ref-rgv-eng": 67,
+            //         "fb-ref-corpus": 68,
+            //         "fb-ref-personal": 69,
+            //     },
+            // },
             areaFilesys: {
                 tabName: "Area Filesys",
                 headerRow: 0,
