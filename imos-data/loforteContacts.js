2988c27af53dfabe655fe01cae9c13c97d90f4fb
ElderLoForte <122490749+ElderLoForte@users.noreply.github.com>
2023-01-11 22:36:27 +0000

    le bleh

--- imos-data/loforteContacts.js
+++ imos-data/loforteContacts.js
@@ -3,6 +3,11 @@ function getContact() {
     let group = ContactsApp.getContactGroup('IMOS Roster'); // Fetches group by groupname 
     let contacts = group.getContacts();                     // Fetches contact list of group 
 
+    // @ts-ignore
+    console.log(contacts.length);
 
+    for (let i = 0; i < contacts.length; i++) {
+        
+    }
 } // end getContact
 
