588ee5be94a2cc414b6704780191f495565e57a1
ElderLoForte <122490749+ElderLoForte@users.noreply.github.com>
2023-01-11 22:38:56 +0000

    made it just it

--- imos-data/loforteContacts.js
+++ imos-data/loforteContacts.js
@@ -4,7 +4,7 @@ function getContact() {
     let contacts = group.getContacts();                     // Fetches contact list of group 
 
     // @ts-ignore
-    console.log(contacts.length);
+    console.log(contacts[1]);
 
     for (let i = 0; i < contacts.length; i++) {
         
