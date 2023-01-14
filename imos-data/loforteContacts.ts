034c4bd463ab2876fb3bc12c4ba39c0de8387bb6
ElderLoForte <122490749+ElderLoForte@users.noreply.github.com>
2023-01-14 22:40:20 +0000

    still just messing around!!!

--- imos-data/loforteContacts.ts
+++ imos-data/loforteContacts.ts
@@ -1,25 +1,226 @@
+// @ts-nocheck
 // Compiled using undefined undefined (TypeScript 4.9.4)
-
+// Compiled using undefined undefined (TypeScript 4.9.4)
 let group = ContactsApp.getContactGroup('IMOS Roster'); // Fetches group by groupname 
 let contacts = group.getContacts(); // Fetches contact list of group 
 
-    function getContact() {
+let name3 = "";
+
+function getContact() {
+    //console.log(contacts[1]);
+    for (let contact of contacts) {
+        let dateContactGenerated = contact.getLastUpdated();
+        //let areaEmail = contactEmail[0];
+        let areaName = contact.getFamilyName();
+        let name1 = "IDKYet";
+        let position1 = "IDK";
+        let isTrainer1 = "IDK";
+
+        let name2 = "IDKYet";
+        let position2 = "IDK";
+        let isTrainer2 = "IDK";
+        name3 = getName3(contact, 3);
+
+        console.log(name3);
+        //console.log(contact.getEmails()[2].getLabel());
         
-        //console.log(contacts[1]);
-
-        for(let contact of contacts){
-
-            // let nameFull1 = contact.getFullName();
-            // let idk = contact.getHomeAddress();
-            // let contactEmail = contact.getEmailAddresses();
-            // let areaEmail = contactEmail[0];
-            // let email1 = contactEmail[1];
-            // let email2 = contactEmail[2];
 
 
-    
-    console.log(contact[1]);
+        
+
+        let district = contact.getNotes();
+        //console.log(contact);
+
+        let nameFull1 = contact.getFullName();
+        let idk = contact.getHomeAddress();
+        let contactEmail = contact.getEmailAddresses();
+
+
+        
+
+        
+        // let email1 = contactEmail[1];
+        // let email2 = contactEmail[2];
+        // let dates = contact.getDates[0];
+           
+           
 
     } // end forLoop
+
     
+
 } // end getContacts
+
+function getName3 (contact) {
+
+  if(isTreo(contact)) {
+
+          return contact.getEmails()[3].getLabel();
+          } else {
+            return "";
+          } // end else
+
+}
+
+
+function isTreo(contact) {
+
+  if (contact.getEmails()[3] =! undefined) {
+    false
+  } else {
+    true
+  }
+
+} // end isTreo
+
+
+
+
+
+
+function getMissionaryRole () {
+
+  
+  let missionary1 = contact.getEmails()[1].getLabel();
+  let missionary2 = contact.getEmails()[2].getLabel();
+  let missionary3 = contact.getEmails()[3].getLabel();
+
+
+  console.log(missionary1);
+  console.log(missionary2);
+  console.log(missionary3);
+  console.log("end");
+
+
+} // end getMissionaryRole
+
+
+
+
+  // let obj1 = {
+
+  //     nameFull1: "123"
+
+  //   }
+
+//console.log(obj1.nameFull1);
+
+
+
+// function notes() {
+// //console.log( ' contact.getFullName (): ', contact.getFullName ());
+// // console.log( ' contact.getId (): ', contact.getId ());
+// // console.log( ' contact.getAddresses (): ', contact.getAddresses ());
+// // console.log( ' contact.getCompanies (): ', contact.getCompanies ());
+// // console.log( ' contact.getContactGroups (): ', contact.getContactGroups ());
+// // console.log( ' contact.getPrimaryEmail (): ', contact.getPrimaryEmail ());
+// // console.log( ' contact.getGivenName (): ', contact.getGivenName ());
+// // console.log( ' contact.getMiddleName (): ', contact.getMiddleName ());
+// // console.log( ' contact.getFamilyName (): ', contact.getFamilyName ());
+// // console.log( ' contact.getMaidenName (): ', contact.getMaidenName ());
+// // console.log( ' contact.getNickname (): ', contact.getNickname ());
+// // console.log( ' contact.getInitials (): ', contact.getInitials ());
+// // console.log( ' contact.getEmailAddresses (): ', contact.getEmailAddresses ());
+// // console.log( ' contact.getHomeAddress (): ', contact.getHomeAddress ());
+// // console.log( ' contact.getWorkAddress (): ', contact.getWorkAddress ());
+// // console.log( ' contact.getHomePhone (): ', contact.getHomePhone ());
+// // console.log( ' contact.getWorkPhone (): ', contact.getWorkPhone ());
+// // console.log( ' contact.getMobilePhone (): ', contact.getMobilePhone ());
+// // console.log( ' contact.getDates (): ', contact.getDates ());
+// // console.log( ' contact.getEmails (): ', contact.getEmails ());
+// // console.log( ' contact.getNotes (): ', contact.getNotes ());
+// // console.log( ' contact.getIMs (): ', contact.getIMs ());
+// // console.log( ' contact.getPhones (): ', contact.getPhones ());
+// // console.log( ' contact.getUrls (): ', contact.getUrls ());
+// // console.log( ' contact.getPager (): ', contact.getPager ());
+// // console.log( ' contact.getHomeFax (): ', contact.getHomeFax ());
+// // console.log( ' contact.getWorkFax (): ', contact.getWorkFax ());
+// // console.log( ' contact.getLastUpdated (): ', contact.getLastUpdated ());
+// // console.log( ' contact.getCustomFields (): ', contact.getCustomFields ());
+// // console.log( ' contact.getShortName (): ', contact.getShortName ());
+// // console.log( ' contact.getPrefix (): ', contact.getPrefix ());
+// // console.log( ' contact.getSuffix (): ', contact.getSuffix ());
+// // console.log( ' contact.getTitle (): ', contact.getTitle ());
+
+
+
+
+
+
+
+
+
+// // toString 
+// // getFullName 
+// // addDate 
+// // getId 
+// // setFullName 
+// // setPrefix 
+// // getAddresses 
+// // setUserDefinedFields 
+// // getCompanies 
+// // deleteContact 
+// // getContactGroups 
+// // getPrimaryEmail 
+// // setGivenName 
+// // setFamilyName 
+// // getGivenName 
+// // getMiddleName 
+// // setMiddleName 
+// // getFamilyName 
+// // getMaidenName 
+// // setMaidenName 
+// // getNickname 
+// // setNickname 
+// // setShortName 
+// // getInitials 
+// // setInitials 
+// // removeFromGroup 
+// // getEmailAddresses 
+// // setPrimaryEmail 
+// // setHomeAddress 
+// // setWorkAddress 
+// // getHomeAddress 
+// // getWorkAddress 
+// // setHomePhone 
+// // setWorkPhone 
+// // setMobilePhone 
+// // getHomePhone 
+// // getWorkPhone 
+// // getMobilePhone 
+// // getUserDefinedFields 
+// // setUserDefinedField 
+// // getUserDefinedField 
+// // addCustomField 
+// // getDates 
+// // addIM 
+// // setNotes 
+// // addEmail 
+// // addToGroup 
+// // getEmails 
+// // getNotes 
+// // getIMs 
+// // getPhones 
+// // addPhone 
+// // getUrls 
+// // addUrl 
+// // addCompany 
+// // setPager 
+// // setHomeFax 
+// // setWorkFax 
+// // getPager 
+// // getHomeFax 
+// // getWorkFax 
+// // addAddress 
+// // getLastUpdated 
+// // setSuffix 
+// // getCustomFields 
+// // getShortName 
+// // getPrefix 
+// // getSuffix 
+// // getTitle 
+// // compareTo 
+// what the heck
+
+
+// }
