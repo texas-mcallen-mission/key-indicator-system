b2d8953ff613b3a066154f8a09b26a1999a93473
harrierpigeon <32232925+HarrierPigeon@users.noreply.github.com>
2023-01-24 16:25:45 -0600

    added reportUpdater test

--- aa-shortcuts.js
+++ aa-shortcuts.js
@@ -2,30 +2,40 @@
 // Shortcut functions just to make full-system testing from GAS easier
 
 
-
-function run_updateDataSheet() {
-    updateDataSheet();
-}
-
 function run_importContacts() {
     const allSheetData = constructSheetData();
     importContacts(allSheetData);
 }
 
-function run_shareFileSys() {
-    shareFileSystem();
+function run_updateDataSheet() {
+    updateDataSheet();
 }
 
-function run_updateReports() {
-    updateAreaReports();
-    updateDistrictReports();
-    updateZoneReports();
+
+function run_shareFileSys() {
+    shareFileSystem();
 }
 
 function run_markDuplicates() {
     const allSheetData = constructSheetData();
     markDuplicates(allSheetData);
 }
+
+function run_reportTester() {
+    createMissingReports()
+    areaShardUpdater1();
+    districtShardUpdater1();
+    zoneShardUpdater1();
+    console.log("updated shards for area, district and zone.")
+}
+
+function run_updateReports() {
+    // note: this will take a VERY long time
+    updateAreaReportsV5();
+    updateDistrictReportsV5();
+    updateZoneReportsV5();
+}
+
 
 function run_updateForm() {
     updateForm();
@@ -39,6 +49,9 @@ function clearCache() {
 
 function run_constructSheetData() {
     const allSheetData = constructSheetData();
+    for (const key in allSheetData) {
+        console.log(key)
+    }
 }
 
 function setUpTriggers() {
--- docs/full-syschecklist.md
+++ docs/full-syschecklist.md
@@ -6,8 +6,20 @@ I honestly don't know how I would automate this, so here's a written out guide o
 
 - This thing is pretty bulletproof, but to make sure everything is configured properly, consider deleting some of the sheets that are automatically generated and see what happens.
 
-
 ## IMOS Data
 
 - delete the contactData sheet and run ``run_importContacts`` from ``aa-shortcuts.gs``
-- 
+- verify that ``contactData`` sheet exists and that the areas and information are correct
+
+## Data Flow
+
+- add an entry to form responses.  (You can create this manually in Sheets, submit a form, copy-paste an entry, or just clear the value stored in ``isPulled``)
+- Make sure that ``skip_marking_duplicates`` is set to false if you're on a live release version
+- run ``run_updateDataSheet`` from ``aa-shortcuts.gs``.
+  - if you get an error, make sure that your area names are up to date with ones in Contact Data
+
+## Report Updater
+
+- Run ``run_reportTester`` in ``aa-shortcuts.gs``
+- This will take a minute because it'll create templates for *all* areas, districts, and zones
+- Check the execution log to see what groups updated, and look at a report from each of the three report types.
