8ddbec533efad424ece946719b5c641d5415d7a7
ElderLoForte <122490749+ElderLoForte@users.noreply.github.com>
2023-01-24 22:53:45 +0000

    fixed a lot of stuff got all the info there just cleaning it up now making it faster....

--- imos-data/loforteContacts.ts
+++ imos-data/loforteContacts.ts
@@ -3,6 +3,7 @@
 // Compiled using undefined undefined (TypeScript 4.9.4)
 // Compiled using undefined undefined (TypeScript 4.9.4)
 // Compiled using undefined undefined (TypeScript 4.9.4)
+// Compiled using undefined undefined (TypeScript 4.9.4)
 // @ts-nocheck
 // Compiled using undefined undefined (TypeScript 4.9.4)
 // Compiled using undefined undefined (TypeScript 4.9.4)
@@ -18,33 +19,40 @@ function wrapper_boi() {
             dateContactGenerated: 0,
             areaEmail: 1,
             areaName: 2,
+
             name1: 3,
             position1: 4,
             isTrainer1: 5,
+
             name2: 6,
             position2: 7,
             isTrainer2: 8,
+
             name3: 9,
             position3: 10,
             isTrainer3: 11,
+
             district: 12,
             zone: 13,
+
             unitString: 14,
             hasMultipleUnits: 15,
             languageString: 16,
+
             isSeniorCouple: 17,
             isSisterArea: 18,
+
             hasVehicle: 19,
             vehicleMiles: 20,
             vinLast8: 21,
-            aptAddress: 22,
+
+            aptAddress: 22
         },
     };
     let loForteContacts = new SheetData(new RawSheetData(configData));
     //let data = getContact();
     loForteContacts.setData(writeArray());
 }
-
 function writeArray() {
     let array1 = [];
     for (let contact of contacts) {
@@ -52,62 +60,166 @@ function writeArray() {
     }
     return array1;
 } // end wirteArray
-
 function writeObject(contact) {
     let dateContactGenerated = contact.getLastUpdated();
     let areaEmail = contact.getEmails()[0].getAddress();
     let areaName = contact.getFamilyName();
-    let name1 = getName(contact,1);
-    //let name2 = getName(contact,2);
-    let position1 = getPosition(contact, 1);
+
+    let name1 = getName(isSolo(contact), contact, 1);
+    let position1 = getPosition(isSolo(contact), contact, 1);
     let isTrainer1 = isTrainer(position1);
-    let zone = getWhere(contact, 2);
+
+    let name2 = getName(isSolo(contact),contact, 2)
+    let position2 = getPosition(isSolo(contact), contact, 2);
+    let isTrainer2 = isTrainer(position2);
+
+    let name3 = getName3(contact, 3);
+    let position3 = getPosition3(isTreo(contact), contact, 3)
+    let isTrainer3 = isTrainer(position3);
+
     let district = getWhere(contact, 1);
+    let zone = getWhere(contact, 2);
+
+    let unitString = getWhere(contact, 3);
+    let hasMultipleUnits = ifMultipleUnitStrings(contact);
+    //let languageString = ""
+
     let isSisterArea = isSisterAreaFunc(contact);
     let isSeniorCouple = isSeniorCoupleFunc(contact);
+
     let hasVehicle = hasVehicleFunc(contact);
     let vehicleMiles = getMiles(hasVehicle, contact);
     let vinLast8 = getVin(hasVehicle, contact);
-    //let aptAddress = contact.getAddresses()[0].getAddress(); // this isnt working!!!!
-    //console.log(aptAddress);
+
+    let aptAddress = getAddress(noAddress(contact), contact) // this isnt working!!!!
+    
     return {
         dateContactGenerated: dateContactGenerated,
         areaEmail: areaEmail,
         areaName: areaName,
+
         name1: name1,
         position1: position1,
-        //name2: name2,
+        isTrainer1: isTrainer1,
+
+        name2: name2,
+        position2: position2,
+        isTrainer2: isTrainer2,
+
+        name3: name3,
+        position3: position3,
+        isTrainer3: isTrainer3,
+
         zone: zone,
         district: district,
+
+        unitString: unitString,
+        hasMultipleUnits: hasMultipleUnits,
+
         isSisterArea: isSisterArea,
         isSeniorCouple: isSeniorCouple,
+
         vehicleMiles: vehicleMiles,
         hasVehicle: hasVehicle,
         vinLast8: vinLast8,
-        //aptAddress: aptAddress,
-        isTrainer1: isTrainer1
+
+        aptAddress: aptAddress
     };
 }
 
-function isTrainer(position) { // another way is to add all to an array and then call the array
-  switch (position) {
-    case "TR":
-    case "DT":
-    case "ZLT":
-      return true
-    default:
-    return false
-  } // end switch
-} // end isTrainer
+function cleanUpStrings() {
 
-function getPosition(c, i) {
-  return c.getEmails()[i].getLabel().toString().split(" ").slice(-1).join(" ").split("(")[1].split(")")[0]; // i = 1 for first person
 }
 
+function noAddress (c) {
+  if (c.getAddresses().length === 0) {
+    return true;
+  } else {
+    return false; 
+  }
+}
+function getAddress (noAddress, c) {
+  if (noAddress) {
+    return "No Address On Record";
+  } else {
+    return c.getAddresses()[0].getAddress().toString().replace("\n", " ").replace("\n", " ");
+  }
+}
+
+function ifMultipleUnitStrings(c) {
+  if (c.getNotes().split("\n")[3].includes(",")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function howManyMissionarys(c) {
+  switch (c.getEmail().length) {
+        case "2":
+          return 0;
+        case "3":
+          return 1;
+        case "4":
+          return 2;
+    } // end switch
+}
+
+function isSolo(c) {
+  if (c.getEmails().length === 2) {
+    return true; 
+  } else {
+    return false;
+  }
+}
+function isTreo(c) {
+    if (c.getEmails().length >= 4) {
+       return true;
+    } else {
+      return false;
+    }
+} // end isTreo
+
+function getName(isSolo, c, i) {
+  //console.log(c.getEmails()[i].getLabel())
+  if (isSolo) {
+    return c.getEmails()[1].getLabel().toString().split(" ").slice(0, -1).join(" ");
+  } else {
+    return c.getEmails()[i].getLabel().toString().split(" ").slice(0, -1).join(" "); // i = 1 for first person
+  }
+} // end getName
+
+function getPosition3 (isTreo, c, i) {
+  if (isTreo) {
+    return c.getEmails()[3].getLabel().toString().split(" ").slice(-1).join(" ").split("(")[1].split(")")[0]; // i = 1 for first person
+  }
+}
+function getName3(c) {
+    if (isTreo(c)) {
+        return getName(isSolo(c), c, 3);
+    } // end if
+} // end getName 3
+
+function isTrainer(position) {
+    switch (position) {
+        case "TR":
+        case "DT":
+        case "ZLT":
+            return true;
+        default:
+            return false;
+    } // end switch
+} // end isTrainer
+function getPosition(isSolo, c, i) {
+  if (isSolo) {
+    return ""
+  } else {
+    return c.getEmails()[i].getLabel().toString().split(" ").slice(-1).join(" ").split("(")[1].split(")")[0]; // i = 1 for first person
+  }    
+}
 function getWhere(c, i) {
     return c.getNotes().split("\n")[i];
 }
-
 // this is what i need to fix
 function getContact() {
     for (let contact of contacts) {
@@ -121,26 +233,17 @@ function hasVehicleFunc(c) {
         return true;
     }
 } // end hasVehicleFunc
-function getName(c, i) {
-        return c.getEmails()[i].getLabel().toString().split(" ").slice(0, -1).join(" "); // i = 1 for first person
-} // end getName
-function getName3(c) {
-    if (isTreo(c)) {
-        return getName(c, 3);
-    } // end if
-} // end getName 3
-function isAreaContact(c) {
-    if (c.getEmails().length == 2) return true;
-}
-function isTreo(c) {
-    if (c.getEmails().length >= 3) return true;
-} // end isTreo
+
 function isSisterAreaFunc(c) {
-    if (c.getNotes().includes("Junior Sister")) return true;
+    if (c.getNotes().includes("Junior Sister"))
+        return true;
 } // end isSisterArea
+
 function isSeniorCoupleFunc(c) {
-    if (c.getNotes().includes("Senior Couple"))  return true;     
+    if (c.getNotes().includes("Senior Couple"))
+        return true;
 } // end isSeniorCoupleFunc
+
 function getMiles(hasCar, c) {
     if (hasCar) {
         for (i = 1; i < 15; i++) {
@@ -159,3 +262,5 @@ function getVin(hasCar, c) {
         } // end for
     } // end if
 } //  end function
+
+//return c.getEmails()[i].getLabel().toString().split(" ").slice(-1).join(" ").split("(")[1].split(")")[0]; // i = 1 for first person
