1c1a76732b5cc4150773f5f6f84cba74713aaf4a
harrierpigeon <32232925+HarrierPigeon@users.noreply.github.com>
2023-01-24 15:06:14 -0600

    RUN IT

--- zz_testFile.ts
+++ zz_testFile.ts
@@ -2,5 +2,6 @@ function annoying_Breaker_of_sanity() {
     console.log("WORDS");
     DriveApp.createFolder("WORDS")
     DriveApp.createFolder("words")
+    //literally just to trigger a re-run
 }
 
