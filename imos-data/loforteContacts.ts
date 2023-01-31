3d361b13f0464ba25d763256b0ac3ec304452fd6
ElderLoForte <122490749+ElderLoForte@users.noreply.github.com>
2023-01-31 23:40:26 +0000

    played with nubmers

--- imos-data/loforteContacts.ts
+++ imos-data/loforteContacts.ts
@@ -98,10 +98,10 @@ function convertToContactData(c:GoogleAppsScript.Contacts.Contact)  {
     object.name1 = c.getEmails()[1].getDisplayName();
 
 
-    if (c.getEmails().length < 3) object.name2 = c.getEmails()[2].getDisplayName();
+    if (c.getEmails().length >= 3) object.name2 = c.getEmails()[2].getDisplayName();
 
 
-    //if (c.getEmails.length < 1) object.name3 = c.getEmails()[3].getDisplayName();
+    if (c.getEmails().length >= 4) object.name3 = c.getEmails()[3].getDisplayName();
 
 
     // getting names
