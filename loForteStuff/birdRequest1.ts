479b26054deba0df69ecbee79355b9bbe46e398f
ElderLoForte <122490749+ElderLoForte@users.noreply.github.com>
2023-02-04 23:19:05 +0000

    added birdRequest1

---
+++ loForteStuff/birdRequest1.ts
@@ -0,0 +1,33 @@
+function runIt() {
+    const kicConfig: sheetDataEntry = {
+        tabName: "BirdReq1",
+        headerRow: 0,
+        initialColumnOrder: {
+            areaName: 0,
+            responsePulled: 1,
+            isDuplicate: 2,
+            formTimestamp: 3,
+            submissionEmail: 4,
+            kiDate: 5,
+            np: 6,
+            sa: 7,
+            bd: 8,
+            bc: 9,
+            rca: 10,
+            rc: 11,
+            serviceHrs: 12,
+            cki: 13,
+        },
+        includeSoftcodedColumns: false
+    };
+
+    const blankSheet = new RawSheetData(kicConfig)
+    const kiDataSheet = new SheetData(blankSheet)
+
+    const allSheetData = constructSheetData()
+    const kiDataSheet2 = allSheetData["data"]
+    const kiDataAll = new kiDataClass(kiDataSheet2.getData())
+
+    kiDataSheet.insertData(kiDataSheet2);
+    
+}
