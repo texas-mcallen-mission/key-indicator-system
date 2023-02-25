b02298b151e22d92910c9729784e8c93b3e241e2
ElderLoForte <122490749+ElderLoForte@users.noreply.github.com>
2023-02-25 22:31:23 +0000

    semi cleaned just fix it up on monday

--- imos-data/loforteContacts.ts
+++ imos-data/loforteContacts.ts
@@ -1,30 +1,24 @@
 function makeSheet(): void {
 
   console.time('Execution Time');
-  
+
   const closedAreasSheet = new SheetData(new RawSheetData(sheetDataConfig.local.closedAreas));
-
   const loForteContacts = new SheetData(new RawSheetData(sheetDataConfig.local.contact));
-  
 
-  
   const ogClass = new kiDataClass(loForteContacts.getData())
- 
-  
-  
-  const newContactsArray = loForteContacts.getAllOfKey("areaEmail");
-  const goodOldData : kiDataEntry = loForteContacts.getData();
-    const allData = goodOldData.getAllOfKey("areaEmail")
-  const newContactData :contactEntry[] = getArrayOfContacts()
+  const newContactData: contactEntry[] = getArrayOfContacts()
   loForteContacts.setData(newContactData);
 
   const newContactClass = new kiDataClass(newContactData);
 
   const newAreaIds = newContactClass.getDataFromKey("areaId");
-  ogClass.removeMatchingByKey("areaId",newAreaIds);
-  ogClass.bulkAppendObject({"deletionDate":convertToSheetDate_(new Date())})
-  const leftovers : kiDataEntry[] = ogClass.end
-  if(leftovers.length >0){
+  ogClass.removeMatchingByKey("areaId", newAreaIds);
+  ogClass.bulkAppendObject({
+    "deletionDate": convertToSheetDate_(new Date())
+  })
+  const leftovers: kiDataEntry[] = ogClass.end
+
+  if (leftovers.length > 0) {
     closedAreasSheet.appendData(leftovers);
   }
 
@@ -32,50 +26,11 @@ function makeSheet(): void {
 
 }
 
-function getOldData() {
-  const activeSheet = SpreadsheetApp.getActive().getSheetByName("Contact Data");
-  const array = [];
-    for (let i = 2; !activeSheet.getRange(i,2).isBlank(); i++) {
-        const cell = activeSheet.getRange(i,2).getValue();
-        array.push(cell);
-    }
-  return array;
-}
-
-function resetObject() : closedAreas {
-  return {
-      areaName: "",
-      areaEmail: "",
-      formTimestamp: "",
-      areaId: "",
-    } 
-}
-
-function compareAreas(oldData : kiDataEntry,newAreas) : closedAreas[] {
-  const oldAreas = oldData.getAllOfKey("areaEmail")
-  const arrayOfObjects : closedAreas[] = [];
-  let differenceArray = oldData.filter(oldData.getKey() => !newAreas.includes(areaId));
-
-
-  let object = resetObject();
-  for (let i = 0; i < differenceArray.length; i++) {
-    object.areaEmail = differenceArray[i];
-    object.areaId = 
-    arrayOfObjects.push(object);
-    object = resetObject();
-  }
-
-return arrayOfObjects;
-
-}
-
-
-
 function getArrayOfContacts(): contactEntry[] {
 
   //Pull in contact data from Google Contacts
-  const group = ContactsApp.getContactGroup('IMOS Roster'); // Fetches group by groupname 
-  const contacts = group.getContacts(); // Fetches contact list of group 
+  const group: GoogleAppsScript.Contacts.ContactGroup = ContactsApp.getContactGroup('IMOS Roster'); // Fetches group by groupname 
+  const contacts: GoogleAppsScript.Contacts.Contact[] = group.getContacts(); // Fetches contact list of group 
 
   const arrayOfContacts: contactEntry[] = [];
   for (const contact of contacts) {
@@ -85,7 +40,7 @@ function getArrayOfContacts(): contactEntry[] {
 } // end wirteArray
 
 
-function convertToContactData(c: GoogleAppsScript.Contacts.Contact) {
+function convertToContactData(c: GoogleAppsScript.Contacts.Contact): contactEntry {
   const object: contactEntry = {
     dateContactGenerated: '',
     areaEmail: '',
@@ -153,7 +108,7 @@ function convertToContactData(c: GoogleAppsScript.Contacts.Contact) {
     if (type.includes("Area")) object.areaName = words;
     if (type.includes("Zone")) object.zone = words;
     if (type.includes("District")) object.district = words;
-    if (type.includes("Ecclesiastical Unit")) object.unitString = words.replaceAll(" ",""); // cant do this need to fix
+    if (type.includes("Ecclesiastical Unit")) object.unitString = words.replaceAll(" ", ""); // cant do this need to fix
     if (type.includes("Ecclesiastical Units")) object.hasMultipleUnits = true;
 
 
