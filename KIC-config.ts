92c3188603ff8172eb96a4a3731dfaab6b1ebdde
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2023-03-01 16:18:12 -0600

    updated config

--- KIC-config.ts
+++ KIC-config.ts
@@ -375,6 +375,7 @@ function getSheetDataConfig(): { local: manySheetDataEntries } {
                 tabName: "Data",
                 headerRow: 0,
                 includeSoftcodedColumns: true,
+                use_iterant:true,
                 keyNamesToIgnore: ["responsePulled", "submissionEmail"],
                 initialColumnOrder: {
                     areaName: 0,
