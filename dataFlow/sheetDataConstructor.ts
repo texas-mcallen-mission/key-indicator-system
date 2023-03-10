7f4c9a7137fde61a7bb0d92ce2e8c051261bad3b
Joseph Robertson <32232925+HarrierPigeon@users.noreply.github.com>
2023-03-10 16:30:53 -0600

    Fix Caching (#156)
    * starting work on cache
    
    * Update base
    
    * Update Base Again (#153)
    
    * first test of caching system
    
    * caching values now have enforced timeout and don't get timer reset without loading first.
    
    * added cache clearing function
    
    * added MIT license
    
    * wrote tester function
    
    * more testing.  I think this might warrant some reorganization
    
    * fixed sheetData cache problems
    
    * more testing!
    
    * need to go clean up sheetCore a bit at some point, too
    
    * More scrubby
    
    * curiosidad
    
    * I can't seem to induce cache misses...?
    
    * I made a bad assumption, oopsies
    
    * updated sheetCore

---
+++ dataFlow/sheetDataConstructor.ts
@@ -0,0 +1,163 @@
+/**
+ * @abstract We work with a lottt of sheets, so we built
+ *  on top of sheetCore to make building lots of sheets faster / easier.
+ */
+
+/**
+ * constructSheetDataV3: creates an allSheetData type object, returns only the ones you need or it return all of them.
+ *
+ * @param {string[] | void} target
+ * @return {*}  {manySheetDatas}
+ * @global
+ */
+function constructSheetDataV3(target: string[] | void) : manySheetDatas {
+    const allSheetData: manySheetDatas = {};
+    let targetSheets:string[] = []
+    if (!target) {
+        targetSheets = Object.keys(sheetDataConfig)
+    } else {
+        targetSheets = [...target]
+    }
+
+    for (const entry of targetSheets) {
+        const cacheData: void | SheetData = getSingleSheetDataFromCache_(entry)
+        if (cacheData) {
+            allSheetData[entry] = cacheData
+        } else {
+            const sheetData = new SheetData(new RawSheetData(sheetDataConfig[entry]))
+            allSheetData[entry] = sheetData
+            cacheSingleSheetDataEntry_(sheetData,entry)
+        }
+    }
+    // cacheSheetDataEntries_(allSheetData)
+    return allSheetData;
+}
+
+const sdCacheString = "SHEETCACHE_"
+
+// /**
+//  * @description stores sheetData entries in the AppsScript cache for faster performance.
+//  * @param {manySheetDatas} anySheetData
+//  */
+// function cacheSheetDataEntries_(anySheetData: manySheetDatas,timeoutInMinutes=15) {
+//     const cache = CacheService.getScriptCache()
+//     const timeout = timeoutInMinutes * 60
+//     // first: update cache for given entries
+//     for (const key in anySheetData) {
+//         const cacheKey = sdCacheString + key
+//         const cacheDataObj = anySheetData[key].getConfigForCache()
+//         if (!isSheetAlreadyCached_(cacheKey)) {
+//             const cacheData = JSON.stringify(cacheDataObj)
+//             cache.put(cacheKey,cacheData,timeout)
+//         } else {
+//             console.log("skipped caching to enforce caching timeout")
+//         }
+
+//     }
+
+// }
+
+/**
+ * @description stores a sheetData entry in the AppsScript cache for faster performance.
+ * @param {SheetData} sheetData
+ * @param {string} name
+ * @param {number} [timeoutInMinutes=15]
+ */
+function cacheSingleSheetDataEntry_(sheetData: SheetData, name: string, timeoutInMinutes = 15) {
+    const cache = CacheService.getScriptCache()
+    const timeout = timeoutInMinutes * 60
+    
+    const cacheKey = sdCacheString + name
+    const cacheDataObj = JSON.stringify(sheetData.getConfigForCache())
+    cache.put(cacheKey,cacheDataObj,timeout)
+}
+
+function isSheetAlreadyCached_(target: string): boolean{
+    // let returnVal = false
+    
+    const cache = CacheService.getScriptCache()
+    const cacheKey = sdCacheString + target
+    const cacheData = cache.get(cacheKey)
+    if (cacheData) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+/**
+ * @description loads a sheetData entry from cache, if it exists, otherwise return void.
+Returning void allows you to test 
+ * @param {string} target
+ * @return {*}  {(SheetData|void)}
+ */
+function getSingleSheetDataFromCache_(target: string):SheetData|void {
+    const cache = CacheService.getScriptCache()
+    const cacheKey = sdCacheString + target
+    const cacheData = cache.get(cacheKey)
+    if (cacheData) {
+        const dataEntry: sheetDataEntry = JSON.parse(cacheData)
+        return new SheetData(new RawSheetData(dataEntry))
+    } else {
+        console.warn("sheetDataCache cache miss!")
+        return
+    }
+}
+
+
+/**
+ * @description removes sheetData entries in ``sheetDataConfig`` from cache if they exist.
+ * @global
+ */
+function deleteSheetDatasFromCache() {
+    const cache = CacheService.getScriptCache()
+    const keys = Object.keys(sheetDataConfig)
+    const cacheKeys = []
+    for (const key of keys) {
+        cacheKeys.push(sdCacheString + key)
+    }
+    console.log("Removed",cacheKeys,"from cache")
+    cache.removeAll(cacheKeys)
+}
+
+
+/**
+ * @description basic function to test if allSheetData caching worked
+ * @param {manySheetDatas} allSheetData
+ */
+function testAllSheetData_(allSheetData: manySheetDatas) {
+    for (const key in allSheetData) {
+        const sheetData = allSheetData[key]
+        sheetData.getData()
+        sheetData.getAllOfIndex(1)
+        sheetData.getTabName()
+    }
+}
+
+function testCachingV2() {
+    /**
+     * @description basic timing function thingy for debugging / testing this.
+     * @param {Date} time
+     * @return {*}  {number}
+     */
+    function timeFunction_(time: Date): number {
+        const endTime = new Date();
+        return endTime.getTime() - time.getTime();
+    }
+    deleteSheetDatasFromCache();
+    // clearAllSheetDataCache()
+    // using constructSheetData now caches on the backend
+    // the first call will cache it
+    const startTime = new Date();
+    const allSheetData: manySheetDatas = constructSheetDataV3();
+    const noCacheTime = timeFunction_(startTime);
+    console.log(String(noCacheTime));
+    testAllSheetData_(allSheetData)
+    const start2 = new Date();
+
+    const allSheetData2 = constructSheetDataV3();
+    const cachedTime = timeFunction_(start2);
+    testAllSheetData_(allSheetData2)
+    console.log("uncached duration, ms:", noCacheTime, "cached duration, ms:", cachedTime);
+}
