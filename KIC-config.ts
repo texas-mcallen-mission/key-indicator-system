dfc1ca6f2e855dedea736748e3f0cfee2c96cfe7
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2023-03-11 14:50:29 -0600

    fixed config problem

--- KIC-config.ts
+++ KIC-config.ts
@@ -472,7 +472,6 @@ function getSheetDataConfig(): { local: manySheetDataEntries } {
                 tabName: "DEBUG SHEET",
                 headerRow: 0,
                 includeSoftcodedColumns: true,
-                sheetId:CONFIG.sheetTargets.debug,
                 initialColumnOrder: {
                     functionName: 0,
                     baseFunction: 1,
