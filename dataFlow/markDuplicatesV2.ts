e97d892d3fae407c2d8c81ee3a98a76202aeed7e
Joseph Robertson <32232925+HarrierPigeon@users.noreply.github.com>
2023-03-14 16:59:55 -0500

    Fix Duplicates Again (#166)
    * Removed corrected entries concept
    
    * updated sheetCore
    
    * set default entry position to zero instead of -1 and fixed eqeqeq problem
    
    * I may have done a silly
    
    * cleanup
    
    * updated sheetCore pre-merge

--- dataFlow/markDuplicatesV2.ts
+++ dataFlow/markDuplicatesV2.ts
@@ -74,149 +74,8 @@ function getOldestAndNewestEntry(data:kiDataEntry[],timeKey: string): oldestNewe
     return output
 }
 
-function createDumbEntry() {
-    function randNum() {
-        return Math.floor(Math.random()*5)
-    }
-    const dumbEntry: kiDataEntry = {
-        areaName: "Mercedes C",
-        // responsePulled: 1,
-        // isDuplicate: 2,
-        formTimestamp: convertToSheetDate_(new Date().toString()),
-        submissionEmail: "DEBUGDATA@LOL",
-        kiDate: convertToSheetDate_(new Date().toString()),
-        np: randNum(),
-        sa: randNum(),
-        bd: randNum(),
-        bc: randNum(),
-        rca: randNum(),
-        rc: randNum(),
-        serviceHrs: randNum(),
-        cki: randNum(),
-        "bap-self-ref": randNum(),
-        "bap-street": randNum(),
-        "bap-ward-activity-or-event": randNum(),
-        "bap-ref-recent-convert": randNum(),
-        "bap-ref-part-member": randNum(),
-        "bap-ref-other-member": randNum(),
-        "bap-ref-teaching-pool": randNum(),
-        "bap-ref-other-non-member": randNum(),
-        "bap-fb-mission": randNum(),
-        "bap-fb-personal": randNum(),
-        "bap-family-history": randNum(),
-        "bap-taught-prev": randNum(),
-        "fb-role": 26,
-        "fb-ref-rgv-eng": randNum(),
-        "fb-ref-rgv-spa": randNum(),
-        "fb-ref-laredo-eng": randNum(),
-        "fb-ref-laredo-spa": randNum(),
-        "fb-ref-ysa": randNum(),
-        "fb-ref-asl": randNum(),
-        "fb-ref-service": randNum(),
-        "fb-ref-corpus": randNum(),
-        "fb-ref-personal": randNum(),
-        "feedback-general": "DEBUG DATA", // had to hardcode these because I added more questions afterwards.
-        "feedback-improvement": "DEBUG DATA",
-        "feedback-analysis": "DEBUG DATA!",
-        "fb-ref-st-eng": randNum(),
-        "fb-ref-st-spa": randNum(),
-        "mpl": randNum(),
-        "RCA-weekly": randNum()
-    };
 
-    const areaNames = ['Mission 3A',"Sharyland B","Mercedes C"]
-    const entries:kiDataEntry[] = []
-    for (let i = 0; i < 5; i++){
-        const entry = { ...dumbEntry }
-        entry["areaName"] = areaNames[i % areaNames.length]
-        entries.push(entry)
-    }
-    // const granulify = new kiDataClass(entries).addGranulatedTime("kiDate", "kiDate", timeGranularities.day).end
-    constructSheetDataV3(["form"]).form.appendData(entries)
-}
 
-function testCreateCorrectedEntry() {
-    const areaData = {
-        area: "WORD",
-        isSister: false,
-        ki1: 123,
-    }
-    const kiData = {
-        area: "WORDLE",
-        isSister: true,
-        ki1: 321
-    }
-    const keysToKeep = ["area", "isSister"]
-    const correctionDate = convertToSheetDate_(new Date())
-    const output = createCorrectedEntry_(areaData,kiData,keysToKeep,correctionDate)
-    const comparison = {
-        area: "WORD",
-        isSister: false,
-        ki1: 321
-    }
-    let pass = true
-    for (const key in areaData) {
-        if (comparison[key] != output[key]) {
-            pass = false
-        }
-    }
-    if (pass) {
-        console.log("✅✔✅ PASS ✅✔✅")
-    } else {
-        console.log("❌⛔❌ FAIL ❌⛔❌")
-    }
-}
-
-function getNewestCorrectedEntry_(data: kiDataEntry[]): kiDataEntry | null{
-    let output = null
-    const logKey = "log"
-    const logTimeKey = "newestTime"
-
-    const correctedEntries: kiDataEntry[] = []
-    for (const entry of data) {
-        if (Object.prototype.hasOwnProperty.call(entry, logKey)) {
-            // check to see if there's any corrected entries
-            if (String(entry[logKey]).includes("CORRECTED_ENTRY")) {
-                // if this is the  first that passes the corrected entry check, set output equal to it
-                if (output == null) {
-                    output = entry
-                } else {
-                    // loads up the status log section of the data, and parses it for the latest timestamp inside.
-                    const outputLogData = JSON.parse(output[logKey])
-                    const testLogData = JSON.parse(entry[logKey])
-                    if (Object.hasOwn(outputLogData, logTimeKey) && Object.hasOwn(testLogData, logTimeKey)) {
-                        // so if we *can* make a comparison, do things... otherwise we just kinda yolo it.
-                        if (outputLogData[logTimeKey] < entry[logTimeKey]) {
-                            output = entry
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
-
-    return output
-}
-
-function createCorrectedEntry_(newData: kiDataEntry, areaData: kiDataEntry, keysToKeep: string[],timestamp_key:string) {
-    const output: kiDataEntry = copyObjectNoRecursion_(areaData);
-    for (const key of keysToKeep) {
-        if (Object.hasOwnProperty.call(newData, key)) {
-
-            output[key] = newData[key]
-        }
-    }
-    const logData = {
-        log: "CORRECTED_ENTRY",
-        newestTime:-1
-    }
-    logData.newestTime = new Date(newData[timestamp_key]).getTime()
-    output["log"] = JSON.stringify(logData)
-    output["isDuplicate"] = false
-    return output
-
-}
 
 
 /**
@@ -225,46 +84,7 @@ function createCorrectedEntry_(newData: kiDataEntry, areaData: kiDataEntry, keys
  * @param {number} [weeksToMark=7] Number of weeks: if set to -1 will do all of them.
  */
 function markDuplicatesV2_(dataSheet: SheetData, weeksToMark = 7) {
-    const areaDataKeys = [
-        "areaName",
-        "areaEmail",
-        "areaID",
-        "kiDate",
-        "name1",
-        "position1",
-        "isTrainer1",
-        "name2",
-        "position2",
-        "isTrainer2",
-        "name3",
-        "position3",
-        "isTrainer3",
-        "districtLeader",
-        "zoneLeader1",
-        "zoneLeader2",
-        "zoneLeader3",
-        "stl1",
-        "stl2",
-        "stl3",
-        "stlt1",
-        "stlt2",
-        "stlt3",
-        "assistant1",
-        "assistant2",
-        "assistant3",
-        "district",
-        "zone",
-        "unitString",
-        "hasMultipleUnits",
-        "languageString",
-        "isSeniorCouple",
-        "isSisterArea",
-        "hasVehicle",
-        "vehicleMiles",
-        "vinLast8",
-        "aptAddress",
-        "fb-role"
-    ];
+    
     const dataClass = new kiDataClass(dataSheet.getData());
     // const iterantKey = "add_iterant"
     // dataClass.addIterant(iterantKey,1)
@@ -287,8 +107,7 @@ function markDuplicatesV2_(dataSheet: SheetData, weeksToMark = 7) {
     //@ts-expect-error I'm intentionally abusing this.  Llore.
     const aggData: two_key_grouper = dataClass.groupDataByMultipleKeys([time_key, area_id_key]);
     
-    const correctionEntries: kiDataEntry[] = [];
-    const markAsDuplicateEntries: kiDataEntry[] = [];
+    const olderEntries: kiDataEntry[] = [];
     const okEntries: kiDataEntry[] = [];
     const itkey = dataSheet.iterantKey;
     
@@ -301,49 +120,27 @@ function markDuplicatesV2_(dataSheet: SheetData, weeksToMark = 7) {
             let skip = false;
             if (data.length <= 1) {
                 // skip to next in the for loop
+                okEntries.push(...data)
                 continue;
             }
-            let dataModClass = new kiDataClass(data);
-            let correctedEntries = dataModClass.popStringIncludes("log", "CORRECTED_ENTRY");
-            let relevantEntries = getOldestAndNewestEntry(data, timestamp_key);
-            // first check to see if there are corrected entries
-            // and then if there are any, whether or not we're up to date.
-            if (correctedEntries.length > 0) {
-                let latestCorrectiveEntry = getNewestCorrectedEntry_(correctedEntries);
-                // Corrected entries store some JSON data in the log that we have to use here:
-                let logData = JSON.parse(latestCorrectiveEntry["log"]);
 
-                const logTimeKey = "newestTime";
-
-                // skip if we've already made a corrected entry and it's up to date.
-                const newTime = new Date(relevantEntries.newest.formTimestamp).getTime()
-                const corrTime = new Date(logData[logTimeKey]).getTime()
-                if (corrTime >= newTime) {
-                    continue;
-                } else {
-                    markAsDuplicateEntries.push(...correctedEntries);
-                }
-            }
-
-            // at this point, the only way we're here is if there's stuff that hasn't been updated.
-            const correctedEntry = createCorrectedEntry_(relevantEntries.newest, relevantEntries.oldest, areaDataKeys, timestamp_key);
-            markAsDuplicateEntries.push(...dataModClass.end);
-            correctionEntries.push(correctedEntry);
+            let intDataClass = new kiDataClass(data)
+            okEntries.push(intDataClass.popNewestByDateString(timestamp_key))
+            olderEntries.push(...intDataClass.end)
+            
 
         }
     }
-    // WYLO 2023-03-01: okentries needs to get updated to use the new kiDataEntry format stuff as well.
-    // mark duplicates- I need to figure out a better / more efficient way of doing this...
-    dataSheet.appendData(correctionEntries);
-    // dataSheet.updateRows(markAsDuplicateEntries)
+
     const dupeEntries: kiDataEntry[] = [];
-    for (const entry of markAsDuplicateEntries) {
+    for (const entry of olderEntries) {
         const output: kiDataEntry = {
             isDuplicate: true
         };
         output[itkey] = entry[itkey];
         dupeEntries.push(output);
     }
+
     dataSheet.updateRows(dupeEntries);
     // Mark known good entries as not duplicate.
     const goodEntries: kiDataEntry[] = [];
@@ -355,11 +152,6 @@ function markDuplicatesV2_(dataSheet: SheetData, weeksToMark = 7) {
         goodEntries.push(output);
     }
     dataSheet.updateRows(goodEntries);
-    console.log("Skipped adding ", String(okEntries.length), "entries that were already up to date!");
-    // for (const duplicate of markAsDuplicateEntries) {
-    //     dataSheet.directModify(duplicate, { "isDuplicate": true })
-    // }
-    // for (const notDuplicate of okEntries) {
-    //     dataSheet.directModify(notDuplicate, { "isDuplicate": false })
-    // }
+    console.log("Marked ",dupeEntries.length," entries as duplicate for the last ",weeksToMark," weeks!")
+    
 }
